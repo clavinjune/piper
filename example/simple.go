e9b159f34cc44d79638465059b5f50fe7673a568
clavinjune <24659468+clavinjune@users.noreply.github.com>
2022-12-10 23:30:28 +0700

    add better example
    Signed-off-by: clavinjune <24659468+clavinjune@users.noreply.github.com>

--- example/simple.go
+++ example/simple.go
@@ -17,34 +17,84 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
 	"github.com/clavinjune/piper"
 )
 
-func addOne(m *piper.M[int]) (int, error) {
-	time.Sleep(time.Second)
-	return m.In + 1, nil
+func add65(m *piper.M[int]) (int, error) {
+	name, _ := m.Data["name"].(string)
+	log.Printf("%s: add65(%d)", name, m.In)
+	return m.In + 65, nil
 }
 
-func timesTwo(m *piper.M[int]) (int, error) {
-	time.Sleep(time.Second)
-	return m.In * 2, nil
+func toRune(m *piper.M[int]) (rune, error) {
+	name, _ := m.Data["name"].(string)
+	log.Printf("%s: toRune(%d)", name, m.In)
+	return rune(m.In), nil
+}
+
+func toString(m *piper.M[rune]) (string, error) {
+	name, _ := m.Data["name"].(string)
+	log.Printf("%s: toString(%#q)", name, m.In)
+
+	if m.In > 'Z' {
+		return "", fmt.Errorf("piper example: alphabet out of range %#q", m.In)
+	}
+
+	return string(m.In), nil
 }
 
 func main() {
-	ch, total := piper.From(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-	totalWorker := total / 2
+	// generating 20 - 26
+	nums := make([]int, 7)
+	for i := range nums {
+		nums[i] = i + 20
+	}
+
+	// converts nums to a channel
+	ch, total := piper.From(nums...)
+
+	// use 1/3 of the total data as a worker
+	totalWorker := total / 3
+
+	ctx := context.Background()
+	m := map[string]any{
+		"name": "piper simple example",
+	}
+
+	// create pipeline functions
+	addOneFn := piper.New(ctx, totalWorker, m, add65)
+	toRunFn := piper.New(ctx, totalWorker, m, toRune)
+	toStringFn := piper.New(ctx, totalWorker, m, toString)
 
 	start := time.Now()
-	addOnCh := piper.New(context.Background(), totalWorker, nil, addOne).Do(ch)
-	timesTwoCh := piper.New(context.Background(), totalWorker, nil, timesTwo).Do(addOnCh)
-	result := piper.ToData(timesTwoCh)
 
+	// run pipeline functions
+	addOneCh := addOneFn.Do(ch)
+
+	// Fan-Out
+	toRuneCh1 := toRunFn.Do(addOneCh)
+	toRuneCh2 := toRunFn.Do(addOneCh)
+
+	// Fan-In the toRuneCh
+	// Fan-Out the toStringCh
+	toStringCh1 := toStringFn.Do(toRuneCh1, toRuneCh2)
+	toStringCh2 := toStringFn.Do(toRuneCh1, toRuneCh2)
+
+	// Fan-In
+	result := piper.To(toStringCh1, toStringCh2)
 	for _, r := range result {
-		log.Println(r)
+		log.Println(r.String(), r.Error())
 	}
+
+	// to get the non-error result
+	// result := piper.ToData(toStringCh1, toStringCh2)
+	// for _, r := range result {
+	//	log.Println(r)
+	// }
 
 	log.Println(time.Since(start))
 }
